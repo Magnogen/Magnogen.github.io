141e5472a4d84ec2540138452ca1604915c96b40
hydronesiumScratch <hydronesium@gmail.com>
2017-10-13 08:55:06 +0100

    Update Form.js

--- RennApps/Form.js
+++ RennApps/Form.js
@@ -1,1 +1,19 @@
-[{"type":"textarea","required":true,"label":"Input Renn code:","description":"Input your Renn code into the box then you can click the convert box to convert it!","className":"form-control","name":"code","subtype":"textarea"},{"type":"button","subtype":"submit","label":"Convert!","className":"btn btn-primary","name":"submit","style":"primary"}]
+[
+  {
+    "type":"textarea",
+    "required":true,
+    "label":"Input Renn code:",
+    "description":"Input your Renn code into the box then you can click the convert box to convert it!",
+    "className":"form-control",
+    "name":"code",
+    "subtype":"textarea"
+  },
+  {
+    "type":"button",
+    "subtype":"submit",
+    "label":"Convert!",
+    "className":"btn btn-primary",
+    "name":"submit",
+    "style":"primary"
+  }
+]
