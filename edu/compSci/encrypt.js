7ab369a6708807d7d17424d878db7e479fe088a0
Magnogen <hydronesium@gmail.com>
2018-05-02 12:25:49 +0100

    Update encrypt.js

--- edu/compSci/encrypt.js
+++ edu/compSci/encrypt.js
@@ -1,1 +1,14 @@
-
+function convert() {
+        	var amount = prompt("Enter number from 0 to 26:");
+            if (amount < 0 || amount > 26 || amount == "") {
+            	alert("FAIL! TRY AGAIN!!!");
+            } else {
+            	alert("Converting...")
+            	var text = ""
+            	while (i < document.getElementById("input").value.length) {
+                    text = text + document.getElementById("input").value.charAt(0);
+                    i++;
+                }
+                document.getElementById("input").value = text;
+            }
+        }
