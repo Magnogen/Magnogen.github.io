b107cad0f676085b5c72e97d82344278173bb75b
Magnogen <hydronesium@gmail.com>
2018-05-22 15:53:34 +0100

    Update script.js

--- AppData/script.js
+++ AppData/script.js
@@ -1,8 +1,49 @@
 var parsedUrl = new URL(window.location.href);
-if (parsedUrl.searchParams.get("dark") == "t") {
-  document.getElementById("all").style.backgroundColor = "#222222";
-  document.getElementById("all").style.color = "#eeeeee";
-} else {
-	document.getElementById("all").style.backgroundColor = "#ffffff";
-  document.getElementById("all").style.color = "#000000";
-}
+      if (parsedUrl.searchParams.get("dark") == "t") {
+        document.getElementById("all").style.backgroundColor = "#222222";
+        document.getElementById("all").style.color = "#eeeeee";
+        var links = document.getElementsByTagName("a");
+        for(var i=0;i<links.length;i++) {
+          links[i].style.color = "#eeeeee";
+        } 
+      } else {
+        document.getElementById("all").style.backgroundColor = "#ffffff";
+        document.getElementById("all").style.color = "#000000";
+        var links = document.getElementsByTagName("a");
+        for(var i=0;i<links.length;i++) {
+          links[i].style.color = "#000000";
+        } 
+      }
+      function phizix() {
+        document.getElementById("content").innerHTML = " \
+        <style> \
+          .header { \
+            font-size: 18px; \
+          } \
+        </style> \
+        <span class=\"header\">\
+        Phizix \
+        </span>";
+      }
+      function ytb() {
+        document.getElementById("content").innerHTML = " \
+        <style> \
+          .header { \
+            font-size: 18px; \
+          } \
+        </style> \
+        <span class=\"header\">\
+        YoutuBeats \
+        </span>";
+      }
+      function emotions() {
+        document.getElementById("content").innerHTML = " \
+        <style> \
+          .header { \
+            font-size: 18px; \
+          } \
+        </style> \
+        <span class=\"header\">\
+        Emotions \
+        </span>";
+      }
