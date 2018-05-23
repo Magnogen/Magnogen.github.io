fd1c430c257fe615845357abf56bb715fd074254
Magnogen <hydronesium@gmail.com>
2018-05-23 15:58:44 +0100

    Update music-script.js

--- AppData/music-script.js
+++ AppData/music-script.js
@@ -1,33 +1,37 @@
 function phizix() {
-  document.getElementById("content").innerHTML = " \
-    <style> \
-      .header { \
-        font-size: 18px; \
-      } \
-    </style> \
-    <span class=\"header\">\
-      Phizix \
-    </span>";
-}
-function ytb() {
-  document.getElementById("content").innerHTML = " \
-    <style> \
-      .header { \
-        font-size: 18px; \
-      } \
-    </style> \
-    <span class=\"header\">\
-      YoutuBeats \
-    </span>";
-}
-function emotions() {
-  document.getElementById("content").innerHTML = " \
-    <style> \
-      .header { \
-        font-size: 18px; \
-      } \
-    </style> \
-    <span class=\"header\">\
-      Emotions \
-    </span>";
-}
+      document.getElementById("content").innerHTML = " \
+        <style> \
+          .header { \
+            font-size: 24px; \
+          } \
+        </style> \
+        <span class=\"header\">\
+          Phizix \
+        </span><br> \
+        Select track:<br> \
+        <a class=\"track\" onClick=\"playPhizix(\"1\")\">Automate</a><br> \
+        <a class=\"track\" onClick=\"playPhizix(\"2\")\">Breeze</a><br> \
+        <a class=\"track\" onClick=\"playPhizix(\"3\")\">Renegade</a><br>";
+    }
+    function ytb() {
+      document.getElementById("content").innerHTML = " \
+        <style> \
+          .header { \
+            font-size: 24px; \
+          } \
+        </style> \
+        <span class=\"header\">\
+          YoutuBeats \
+        </span>";
+    }
+    function emotions() {
+      document.getElementById("content").innerHTML = " \
+        <style> \
+          .header { \
+            font-size: 24px; \
+          } \
+        </style> \
+        <span class=\"header\">\
+          Emotions \
+        </span>";
+    }
