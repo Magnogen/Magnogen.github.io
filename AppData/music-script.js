7a77c1b6f157b8c355d012b7eb664d4720b5d1e7
Magnogen <hydronesium@gmail.com>
2018-05-23 15:27:43 +0100

    Update and rename script.js to music-script.js

---
+++ AppData/music-script.js
@@ -0,0 +1,33 @@
+function phizix() {
+  document.getElementById("content").innerHTML = " \
+    <style> \
+      .header { \
+        font-size: 18px; \
+      } \
+    </style> \
+    <span class=\"header\">\
+      Phizix \
+    </span>";
+}
+function ytb() {
+  document.getElementById("content").innerHTML = " \
+    <style> \
+      .header { \
+        font-size: 18px; \
+      } \
+    </style> \
+    <span class=\"header\">\
+      YoutuBeats \
+    </span>";
+}
+function emotions() {
+  document.getElementById("content").innerHTML = " \
+    <style> \
+      .header { \
+        font-size: 18px; \
+      } \
+    </style> \
+    <span class=\"header\">\
+      Emotions \
+    </span>";
+}
