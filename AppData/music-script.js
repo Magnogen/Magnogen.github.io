5db3dafe7dccb3aed81f0d8e5434003f1eed86e2
Magnogen <hydronesium@gmail.com>
2018-06-04 13:45:16 +0100

    Update music-script.js

--- AppData/music-script.js
+++ AppData/music-script.js
@@ -1,37 +1,44 @@
 function phizix() {
       document.getElementById("content").innerHTML = " \
-        <style> \
-          .header { \
+            <style> \
+            .header { \
             font-size: 24px; \
-          } \
-        </style> \
-        <span class=\"header\">\
-          Phizix \
-        </span><br> \
-        Select track:<br> \
-        <a class=\"track\" onClick=\"playPhizix(\"1\")\">Automate</a><br> \
-        <a class=\"track\" onClick=\"playPhizix(\"2\")\">Breeze</a><br> \
-        <a class=\"track\" onClick=\"playPhizix(\"3\")\">Renegade</a><br>";
-    }
-    function ytb() {
+            } \
+            </style> \
+            <span class=\"header\">\
+            Phizix \
+            </span><br> \
+            Select track:<br> \
+            <a class=\"track\" onClick=\"playPhizix(\"1\")\">Automate</a><br> \
+            <a class=\"track\" onClick=\"playPhizix(\"2\")\">Breeze</a><br> \
+            <a class=\"track\" onClick=\"playPhizix(\"3\")\">Renegade</a><br> \
+      ";
+}
+function ytb() {
       document.getElementById("content").innerHTML = " \
-        <style> \
-          .header { \
+            <style> \
+            .header { \
             font-size: 24px; \
-          } \
-        </style> \
-        <span class=\"header\">\
-          YoutuBeats \
-        </span>";
-    }
-    function emotions() {
+            } \
+            </style> \
+            <span class=\"header\">\
+            YoutuBeats \
+            </span><br> \
+            Select track:<br> \
+            <a class=\"track\" onClick=\"playYtb(\"1\")\">Vsauce</a><br> \
+      ";
+}
+function emotions() {
       document.getElementById("content").innerHTML = " \
-        <style> \
-          .header { \
+            <style> \
+            .header { \
             font-size: 24px; \
-          } \
-        </style> \
-        <span class=\"header\">\
-          Emotions \
-        </span>";
-    }
+            } \
+            </style> \
+            <span class=\"header\">\
+            Emotions \
+            </span><br> \
+            Select track:<br> \
+            <a class=\"track\" onClick=\"playEmotions(\"1\")\">Laughter</a><br> \
+      ";
+}
