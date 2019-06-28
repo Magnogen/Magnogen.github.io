f382014bec46dee9177ea552311cc54dd896e439
Magnogen <hydronesium@gmail.com>
2019-06-28 11:38:24 +0100

    Create allSongs.js

---
+++ music/search/allSongs.js
@@ -0,0 +1,16 @@
+var tracks = [
+	["automate", "Automate", "Magnogen", "Unspecified", ["old", "style", "alan", "walker", "automate", "unspecified", "planet"]],
+	["breeze", "Breeze", "Magnogen", "Unspecified", ["old", "style", "breeze", "unspecified", "planet", "calm"]],
+	["vsauce", "Vsauce", "Magnogen", "Unspecified", ["unspecified", "old", "style", "vsauce", "spacey", "space", "youtube", "channel", "hey", "calm"]],
+	["renegade", "Renegade", "Magnogen", "Electro", ["planet", "electro", "old", "style", "renegade", "electro", "electronic", "electric", "8bit"]],
+	["scars", "Scars [Magnogen remix]", "Zyzyx", "Lofi", ["lofi", "calm", "old", "style", "zyzyx", "remix", "remixes", "scars", "bass"]],
+	["laughter", "Laughter", "Magnogen", "Electro", ["emotions", "emotion", "laughter", "laugh", "mario", "game", "electro", "electronic", "electric", "8bit", "upbeat", "fast", "happy"]],
+	["adrenaline", "Adrenaline", "Magnogen", "Hardcore", ["hardcore", "hard", "adrenaline", "fast", "singles", "electro", "bass"]],
+	["impurity", "Impurity", "Magnogen", "DnB/Hardcore/Future Bass", ["genres", "genre", "flavour", "full", "drum", "bass", "future", "dnb", "hard", "hardcore"]],
+	["insane", "Insane", "Magnogen", "Drumstep", ["insane", "drum", "step", "dub", "dubstep", ""]],
+	["radius", "Radius", "Magnogen", "Chillstep", ["radius", "chillstep", "chill", "dub", "step", "dubstep", "relaxed", "calm"]],
+	["heartbeat", "Heartbeat", "Magnogen", "Trance", ["heartbeat", "trance", "chill", "psytrance", "psy", "heart", "beat", "electronic", "electro"]],
+	["dejavu", "Déjà vu", "Magnogen", "Trap", ["deja", "déjà", "vu", "trap", "dejavu", "bass", "heavy"]],
+	["avocado", "Avocado", "Magnogen", "Deathstep", ["death", "dub", "step", "dubstep", "deathstep", "avocado", "choir", "ahh", "spooky"]],
+	["pixelate", "Pixelate", "Magnogen", "Electro", ["squres", "pixel", "pixels", "pixelate", "electro", "electronic", "electric", "8bit"]]
+]
