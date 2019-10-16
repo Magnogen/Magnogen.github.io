17bd87a92bb8050c4fe08d446b0609d188ced5cd
Magnogen <hydronesium@gmail.com>
2019-10-16 09:25:37 +0100

    Update allSongs.js

--- music/allSongs.js
+++ music/allSongs.js
@@ -1,18 +1,18 @@
 var allTracks = [
-	["automate", "Automate", "Magnogen", "Unspecified", ["old", "style", "alan", "walker", "automate", "unspecified", "planet"]],
-	["breeze", "Breeze", "Magnogen", "Unspecified", ["old", "style", "breeze", "unspecified", "planet", "calm"]],
-	["vsauce", "Vsauce", "Magnogen", "Unspecified", ["unspecified", "old", "style", "vsauce", "spacey", "space", "youtube", "channel", "hey", "calm"]],
-	["renegade", "Renegade", "Magnogen", "Electro", ["planet", "electro", "old", "style", "renegade", "electro", "electronic", "electric", "8bit"]],
-	["scars", "Scars [Magnogen remix]", "Zyzyx", "Lofi", ["lofi", "calm", "old", "style", "zyzyx", "remix", "remixes", "scars", "bass"]],
-	["laughter", "Laughter", "Magnogen", "Electro", ["emotions", "emotion", "laughter", "laugh", "mario", "game", "electro", "electronic", "electric", "8bit", "upbeat", "fast", "happy"]],
-	["adrenaline", "Adrenaline", "Magnogen", "Hardcore", ["hardcore", "hard", "adrenaline", "fast", "singles", "electro", "bass"]],
-	["impurity", "Impurity", "Magnogen", "DnB/Hardcore/Future Bass", ["genres", "genre", "flavour", "full", "drum", "bass", "future", "dnb", "hard", "hardcore"]],
-	["insane", "Insane", "Magnogen", "Drumstep", ["insane", "drum", "step", "dub", "dubstep", ""]],
-	["radius", "Radius", "Magnogen", "Chillstep", ["radius", "chillstep", "chill", "dub", "step", "dubstep", "relaxed", "calm"]],
-	["heartbeat", "Heartbeat", "Magnogen", "Trance", ["heartbeat", "trance", "chill", "psytrance", "psy", "heart", "beat", "electronic", "electro"]],
-	["dejavu", "Déjà Vu", "Magnogen", "Trap", ["deja", "déjà", "vu", "trap", "dejavu", "bass", "heavy"]],
-	["avocado", "Avocado", "Magnogen", "Deathstep", ["death", "dub", "step", "dubstep", "deathstep", "avocado", "choir", "ahh", "spooky"]],
-	["pixelate", "Pixelate", "Magnogen", "Electro", ["squres", "pixel", "pixels", "pixelate", "electro", "electronic", "electric", "8bit"]]
+	["automate", "Automate", "Magnogen", "Unspecified", ["everything", "old", "style", "alan", "walker", "automate", "unspecified", "planet"]],
+	["breeze", "Breeze", "Magnogen", "Unspecified", ["everything", "old", "style", "breeze", "unspecified", "planet", "calm"]],
+	["vsauce", "Vsauce", "Magnogen", "Unspecified", ["everything", "unspecified", "old", "style", "vsauce", "spacey", "space", "youtube", "channel", "hey", "calm"]],
+	["renegade", "Renegade", "Magnogen", "Electro", ["everything", "planet", "electro", "old", "style", "renegade", "electro", "electronic", "electric", "8bit"]],
+	["scars", "Scars [Magnogen remix]", "Zyzyx", "Lofi", ["everything", "lofi", "calm", "old", "style", "zyzyx", "remix", "remixes", "scars", "bass"]],
+	["laughter", "Laughter", "Magnogen", "Electro", ["everything", "emotions", "emotion", "laughter", "laugh", "mario", "game", "electro", "electronic", "electric", "8bit", "upbeat", "fast", "happy"]],
+	["adrenaline", "Adrenaline", "Magnogen", "Hardcore", ["everything", "hardcore", "hard", "adrenaline", "fast", "singles", "electro", "bass"]],
+	["impurity", "Impurity", "Magnogen", "DnB/Hardcore/Future Bass", ["everything", "genres", "genre", "flavour", "full", "drum", "bass", "future", "dnb", "hard", "hardcore"]],
+	["insane", "Insane", "Magnogen", "Drumstep", ["everything", "insane", "drum", "step", "dub", "dubstep", ""]],
+	["radius", "Radius", "Magnogen", "Chillstep", ["everything", "radius", "chillstep", "chill", "dub", "step", "dubstep", "relaxed", "calm"]],
+	["heartbeat", "Heartbeat", "Magnogen", "Trance", ["everything", "heartbeat", "trance", "chill", "psytrance", "psy", "heart", "beat", "electronic", "electro"]],
+	["dejavu", "Déjà Vu", "Magnogen", "Trap", ["everything", "deja", "déjà", "vu", "trap", "dejavu", "bass", "heavy"]],
+	["avocado", "Avocado", "Magnogen", "Deathstep", ["everything", "death", "dub", "step", "dubstep", "deathstep", "avocado", "choir", "ahh", "spooky"]],
+	["pixelate", "Pixelate", "Magnogen", "Electro", ["everything", "squres", "pixel", "pixels", "pixelate", "electro", "electronic", "electric", "8bit"]]
 ]
 var ids = [
 	"automate",
@@ -28,7 +28,7 @@ var ids = [
 	"heartbeat",
 	"dejavu",
 	"avocado",
-	"pixelate",
+	"pixelate"
 ]
 var soundcloudLinks = [
 	"634433973",
@@ -44,5 +44,5 @@ var soundcloudLinks = [
 	"643406646",
 	"634465800",
 	"643405845",
-	"643407042",
+	"643407042"
 ]
