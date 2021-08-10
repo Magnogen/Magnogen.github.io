8b8ed864b38f1a91804e74a6437aa3bf9cbfdf97
Magnogen <hydronesium@gmail.com>
2021-08-10 11:03:25 +0100

    Update navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -1,5 +1,5 @@
 let me = document.querySelector("[nav]");
-let path = me.getAttribute('path').split(/[\/\\]/);
+let path = me.getAttribute('path').replace(/\s+/g, '').split(/[\/\\]/);
 let rules = {};
 for (let rule of me.getAttribute('rules').split(","))
   rules[rule.split(":")[0]] = rule.split(":")[1];
@@ -8,9 +8,9 @@ let html = "", upto = "";
 for (let node of path) {
   let css = "";
   if (rules.hasOwnProperty(node)) {
-    if (rules[node].charAt(0) == ".")
-      css = rules[node].slice(1);
-    else upto += rules[node] + "/";
+    if (rules[node].includes("."))
+      css = rules[node].split(".")[1];
+    upto += rules[node].split(".")[0] + "/";
   } else upto += node + "/";
   html += `<a href="${upto.slice(0, -1)}"${css == "" ? "" : ` class="${css}"`}>${node}</a>/`;
 }
