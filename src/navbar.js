c68ceaf6fb845616e17c07e59b4a20d1453ed6a3
Magnogen <hydronesium@gmail.com>
2021-11-09 13:18:53 +0000

    fixes, additions and formatting
    Attempt a fix for navbar on mobile;
    add a canvas to the header, with particles in a simple attractor field;
    Format a bunch of stuff for ease of use

--- src/navbar.js
+++ src/navbar.js
@@ -1,22 +1,26 @@
-let me = document.querySelector("script[nav]");
-let path = me.getAttribute('path')
-             .split(/\s+[\/]\s+/);
-let rules = {};
-for (let rule of me.getAttribute('rules').split(","))
-  rules[rule.split(":")[0]] = rule.split(":")[1];
+(_=>{
+  let me = document.currentScript;
+  let path = me.getAttribute('path')
+               .split(/\s+[\/]\s+/);
+  let rules = {};
+  for (let rule of me.getAttribute('rules').split(","))
+    rules[rule.split(":")[0]] = rule.split(":")[1];
 
-let html = "", upto = "";
-for (let node of path) {
-  let css = "";
-  if (rules.hasOwnProperty(node)) {
-    if (rules[node].includes("."))
-      css = rules[node].split(".")[1];
-    upto += (rules[node].split(".")[0]=="_"?node:rules[node].split(".")[0]) + "/";
-  } else upto += node + "/";
-  html += `<a href="${upto.slice(0, -1).toLowerCase()}"${css == "" ? "" : ` class="${css}"`}>${node}</a>/`;
-}
+  let html = "", upto = "";
+  for (let node of path) {
+    let css = "";
+    if (rules.hasOwnProperty(node)) {
+      if (rules[node].includes("."))
+        css = rules[node].split(".")[1];
+      upto += (rules[node].split(".")[0]=="_"?node:rules[node].split(".")[0]) + "/";
+    } else upto += node + "/";
+    html += `<a href="${upto.slice(0, -1).toLowerCase()}"${css == "" ? "" : ` class="${css}"`}>${node}</a>/`;
+  }
 
-html = `<div nav><nav>${html.slice(0, -1)}</nav></div>`;
+  html = `<div nav><nav>${html.slice(0, -1)}</nav></div>`;
 
-me.insertAdjacentHTML("afterend", html);
-me.remove();
+  me.insertAdjacentHTML("afterend", html);
+  me.remove();
+})();
+
+
