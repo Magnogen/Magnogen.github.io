028036603dc6a96165b00dc157641d967256ccc3
Magnogen <hydronesium@gmail.com>
2024-11-03 17:59:17 +0000

    bg pattern texture thing

---
+++ bg.js
@@ -0,0 +1,23 @@
+on('load', () => {
+  
+  const c = $('#bg');
+  const ctx = c.getContext('2d');
+  c.width = 16;
+  c.height = 16;
+  
+  const lerp = (a, b, t) => a + (b-a)*t;
+  
+  const img = ctx.getImageData(0, 0, c.width, c.height);
+  for (let x = 0; x < c.width; x++) {
+    for (let y = 0; y < c.height; y++) {
+      const i = x + y * c.width;
+      const v = rand();
+      img.data[4*i + 0] = lerp(0x20, 0x28, v);
+      img.data[4*i + 1] = lerp(0x20, 0x28, v);
+      img.data[4*i + 2] = lerp(0x20, 0x28, v);
+      img.data[4*i + 3] = 255;
+    }
+  }
+  ctx.putImageData(img, 0, 0);
+  
+});
