e0570dc40eda03ea58749deaccc236fdf5c646cb
Magnogen <hydronesium@gmail.com>
2026-07-19 17:56:46 +0100

    remove a bunch of stuff i dont support anymore, and restyle the home page

--- music/thumbs/resize.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image as im
-
-while True:
-    fp = input("name: ")
-    size = 512, 512
-
-    data = im.open(fp + ".png", mode="r")
-    data.thumbnail(size, im.ANTIALIAS)
-    data.save(fp + "_thumb.png")
